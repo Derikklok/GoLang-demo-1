27cab0bab0a5efbfc546775b0b29e437610ab673
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-09-19 12:36:06 +0530

    Creating variables

--- index.go
+++ index.go
@@ -1,10 +1,21 @@
 package main
+
 // Means that the file beongs to the main package
 import (
 	"fmt"
 )
+
 // fmt - formatted input output
 
+var speed float32 = 234.45
+var name string = "sachin"
+
 func main() {
+
+	var age int
+
 	fmt.Println("Hello World!")
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(speed)
 }
